129730a1df2d8729b68f4ca0f8b0cec25f8630b7
lelanlima <marcelanasc.lima@gmail.com>
2022-09-06 21:04:21 -0300

    alteração do e_commerce/forms.py, e_commerce/views e template/contact/view.html

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .forms import ContactForm
+
 
 def home_page(request):
     context = {
@@ -19,10 +21,16 @@ def about_page(request):
 
 
 def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
     context = {
+
         "title": "Página de contato",
-        "content": "Bem-vindo a página de contato"
+        "content": "Bem-vindo a página de contato",
+        "form": contact_form
     }
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+
     if request.method == "POST":
         print(request.POST)
     return render(request, "contact/view.html", context)
