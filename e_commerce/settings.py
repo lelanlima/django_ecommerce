5815096096f7ca6dee2c63c8a997e1c354c3f8f0
lelanlima <marcelanasc.lima@gmail.com>
2022-09-15 20:57:48 -0300

    Correção de STATIC_URL = '/static/', Alteração de e_commerce/settings e template/home_page, Criação de static_local/css/main.css e static_local/img/imagens-ecommerce.png

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -116,12 +116,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static_local")
 ]
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
+
 MEDIA_URL = '/media/'
+
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
 
 # Default primary key field type
